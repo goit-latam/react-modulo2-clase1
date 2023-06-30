58a1c2efd8aaaadd2f025c55c0346775f55ea96f
goit-latam <126070957+goit-latam@users.noreply.github.com>
2023-06-30 18:28:46 +0200

    done

---
+++ src/components/App.js
@@ -0,0 +1,27 @@
+import { Component } from 'react';
+import { RecipeList } from './RecipeList/RecipeList';
+import initialRecipes from '../recipes.json';
+import { GlobalStyle } from './GlobalStyle';
+import { Layout } from './Layout/Layout';
+
+export class App extends Component {
+  state = {
+    recipes: initialRecipes,
+  };
+
+  deleteRecipe = recipeId => {
+    this.setState(prevState => ({
+      recipes: prevState.recipes.filter(recipe => recipe.id !== recipeId),
+    }));
+  };
+
+  render() {
+    return (
+      <Layout>
+        <RecipeList items={this.state.recipes} onDelete={this.deleteRecipe} />
+
+        <GlobalStyle />
+      </Layout>
+    );
+  }
+}
